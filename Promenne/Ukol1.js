f3630c6d9a5cf47accab8c8c9996e75c69df0db8
kamalaro <ka.malarova@gmail.com>
2022-03-16 11:49:31 +0100

    1 file added

---
+++ Promenne/Ukol1.js
@@ -0,0 +1,8 @@
+//Vypočti počet minut v celém lednu. Vypiš výsledek na konzoli.
+
+let minutVHodine = 60;
+let hodinVeDne = 24;
+let dnuVLednu = 31;
+
+let vysledek = minutVHodine * hodinVeDne * dnuVLednu;
+console.log(vysledek);
